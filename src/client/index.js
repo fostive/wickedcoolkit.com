2600906bd84833391b7f52873b5ec4bd8464d1c3
Luke Karrys <lukekarrys@gmail.com>
2020-12-07 18:01:42 -0700

    fix production check

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import { createElement } from 'lwc';
 import MyApp from 'my/app';
 
-window.PRODUCTION = window.location.host.indexOf('localhost') > -1;
+window.PRODUCTION = window.location.host.indexOf('localhost') === -1;
 
 const app = createElement('my-app', { is: MyApp });
 // eslint-disable-next-line @lwc/lwc/no-document-query
