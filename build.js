668a8b7c7933c745ec022ec2a947a9cfd6eb6539
Luke Karrys <lukekarrys@gmail.com>
2021-01-26 12:58:52 -0700

    build files

--- build.js
+++ build.js
@@ -1,45 +1,63 @@
-const fs = require('fs');
+const fs = require("fs");
+const Handlebars = require("handlebars");
+const path = require("path");
 
-const v = `^1`;
-const unpkgHost = `https://unpkg.com/wicked-coolkit${v ? `@${v}` : ''}/dist`;
-const stickersHost = `${unpkgHost}/stickers`;
-const png = (name) => `${stickersHost}/png/${name}.png`;
-const svg = (name) => `${stickersHost}/svg/${name}.svg`;
+// TODO: change to ^1 to go live
+const COOLKIT_VERSION = "";
+const USER_REPO = "https://github.com/andyet/wicked-coolkit-user";
+const API_HOST = "wickedcoolkitapi.herokuapp.com";
+const PROD_URL = "https://wickedcoolkit.herokuapp.com";
+const CDN_HOST = "https://unpkg.com/wicked-coolkit";
+const CDN = CDN_HOST + (COOLKIT_VERSION ? `@${COOLKIT_VERSION}` : "") + "/dist";
+
+const sticker = (p) => `${CDN}/stickers/${p}`;
 const script = (name) =>
-    `<script async type="module" src="{{unpkg}}/${name}.js"></script>`;
+  `<script async type="module" src="${CDN}/${name}.js"></script>`;
 
 const stickers = fs
-    .readFileSync('./stickers.csv')
-    .split('\n')
-    .map((line) => {
-        const [alt, name] = line.split(',').map(JSON.parse);
-        return { alt, name };
-    })
-    .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
-    .reduce((acc, s) => {
-        acc[s.name] = {
-            alt: s.alt,
-            svg: svg(s.name),
-            png: png(s.name)
-        };
-        return acc;
-    }, {});
-
-const scripts = ['tradingCard', 'hitCounter', 'webring'].reduce((acc, c) => {
-    const tag = c.replace(/[A-Z]/g, (l) => `-${l.toLowerCase()}`);
-    acc[c] = {
-        script: script(c),
-        tag: `<wck-${tag} host="${apiHost}"></wck-${tag}>`
+  .readFileSync("./stickers.csv")
+  .toString()
+  .split("\n")
+  .filter(Boolean)
+  .map((line) => {
+    const [alt, name] = line.split(",").map(JSON.parse);
+    return { alt, name };
+  })
+  .sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()))
+  .reduce((acc, s) => {
+    const tag = s.name
+      .replace(/&/g, "-")
+      .replace(/-./g, (l) => l[1].toUpperCase());
+    acc[tag] = {
+      alt: s.alt,
+      svg: sticker(`svg/${s.name}.svg`),
+      png: sticker(`png/${s.name}.png`),
     };
     return acc;
+  }, {});
+
+const components = ["tradingCard", "hitCounter", "webring"].reduce((acc, c) => {
+  const tag = c.replace(/[A-Z]/g, (l) => `-${l.toLowerCase()}`);
+  acc[c] = {
+    script: script(c),
+    tag: `<wck-${tag} host="${API_HOST}"></wck-${tag}>`,
+  };
+  return acc;
 }, {});
 
 const locals = {
-    deployUrl:
-        'https://heroku.com/deploy?template=https://github.com/andyet/wicked-coolkit-user',
-    apiHost: 'wickedcoolkitapi.herokuapp.com',
-    prodHost: 'https://wickedcoolkit.herokuapp.com',
-    scripts,
-    stickers,
-    stickersZip: `${stickersHost}/stickers.zip`
+  deployUrl: `https://heroku.com/deploy?template=${USER_REPO}`,
+  prodHost: PROD_URL,
+  components,
+  stickers,
+  stickersZip: sticker("stickers.zip"),
+  dotMin: process.env.NODE_ENV === "production" ? ".min" : "",
 };
+
+const viewsDir = "views";
+const publicDir = "public";
+fs.readdirSync(viewsDir).forEach((v) => {
+  const templateSrc = fs.readFileSync(path.join(viewsDir, v)).toString();
+  const template = Handlebars.compile(templateSrc);
+  fs.writeFileSync(path.join(publicDir, v), template(locals));
+});
